234ebc13feeba95bde57802eecf77844a9c15b08
Enzo Briones <enzo.briones@inacapmail.cl>
2024-01-11 13:02:07 -0300

    Antes de insertar por lote

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -69,8 +69,8 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    // // const pokemon = await this.findOne(id);
-    // // await pokemon.deleteOne();
+    // const pokemon = await this.findOne(id);
+    // await pokemon.deleteOne();
     // return { id };
     const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
 
